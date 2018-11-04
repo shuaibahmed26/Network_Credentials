faaeb171cd71cf2a08b0250d96c7fe08a262fc51
alopez34 <44393066+alopez34@users.noreply.github.com>
2018-11-04 01:17:24 -0400

    edit

--- get_network_credentials.bat
+++ get_network_credentials.bat
@@ -1,6 +1,7 @@
-set list='profiles'
-set list[0]='RWQ78'
+set list[0]=RWQ78
 set list[1]='Verizon-SM-G955U-807E'
+
 rm netcredentials.txt
-Netsh WLAN show profile name=%profiles[0]% key=clear >> net_credentials.txt
-Netsh WLAN show profile name=%profiles[1]% key=clear >> net_credentials.txt
+
+Netsh WLAN show profile name=%list[0]% key=clear >> net_credentials.txt
+Netsh WLAN show profile name=%list[1]% key=clear >> net_credentials.txt
