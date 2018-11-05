5b3d48a66220ebc994239bd7b504f7dca4e49473
alopez34 <44393066+alopez34@users.noreply.github.com>
2018-11-05 01:08:35 -0500

    ""

--- get_network_profiles.bat
+++ get_network_profiles.bat
@@ -1,6 +1,6 @@
 Netsh WLAN show profiles > net_profiles.txt 
 set inputfile=net_profiles.txt 
-for /F "tokens=*" "%%a" in (%inputfile%) do (
+for /F "tokens=*" %%a in (%inputfile%) do (
     rem set /a N+=1
     rem if !N! == 9 (ECHO substitute text) ELSE ECHO %%a
     "test">>%inputfile%
