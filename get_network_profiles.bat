29bfedbed4c308d4e936a0fa94c29eaa6324476a
alopez34 <44393066+alopez34@users.noreply.github.com>
2018-11-05 01:14:06 -0500

    test

--- get_network_profiles.bat
+++ get_network_profiles.bat
@@ -1,9 +1,7 @@
 Netsh WLAN show profiles > net_profiles.txt 
 set inputfile=net_profiles.txt 
 for /F "tokens=*" %%a in (%inputfile%) do (
-    "test">>%inputfile%
-    
-    "%%a">>%inputfile%
+    echo "%%a">>%inputfile%
   )
 pause
 
